7b267664ef04968fe2ebb3a109ca48db591f01be
luli_cbigbig <1066521056@qq.com>
2019-05-23 18:51:49 +0800

    test

---
+++ src/com/neuedu/test/TestOne.java
@@ -0,0 +1,9 @@
+package com.neuedu.test;
+
+public class TestOne {
+    public static void main(String[] args) {
+        int a = 8;
+        System.out.println(a++);
+        System.out.println(++a);
+    }
+}
