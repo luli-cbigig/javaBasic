d90c64f2ee8d2093aa7ec70774c30b971be2ec40
luli_cbigbig <1066521056@qq.com>
2019-05-23 20:31:01 +0800

    test

--- src/com/neuedu/test/TestTwo.java
+++ src/com/neuedu/test/TestTwo.java
@@ -2,6 +2,15 @@ package com.neuedu.test;
 
 public class TestTwo {
     public static void main(String[] args) {
-        System.out.println(1);
+        int a=465;
+        int b=0;
+        while(a != 0){
+            b=a%10;
+            System.out.print(b);
+            //第一次执行b=5，a=46
+            //第二次执行b=6，a=4
+            //第三次执行b=4，a=0 循环结束
+            a=a/10;
+        }
     }
 }
