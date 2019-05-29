3b0948ff48968e52baed06e3f425b905c86d8dd8
luli_cbigbig <1066521056@qq.com>
2019-05-29 08:10:49 +0800

    test

---
+++ src/com/neuedu/test/Student20Test.java
@@ -0,0 +1,42 @@
+package com.neuedu.test;
+
+public class Student20Test {
+    public static void main(String[] args) {
+        Student20 stu = new Student20();
+        stu.setName("A");
+        stu.setNumber("000001");
+        stu.setChineae(80);
+        stu.setEnglish(85);
+        stu.setMath(95);
+        stu.back();
+        Student20 stu1 = new Student20();
+        stu1.setName("B");
+        stu1.setNumber("000002");
+        stu1.setChineae(87);
+        stu1.setEnglish(72);
+        stu1.setMath(95);
+        stu1.back();
+        Student20 stu2 = new Student20();
+        stu2.setName("C");
+        stu2.setNumber("000003");
+        stu2.setChineae(82);
+        stu2.setEnglish(89);
+        stu2.setMath(93);
+        stu2.back();
+        Student20 stu3 = new Student20();
+        stu3.setName("D");
+        stu3.setNumber("000004");
+        stu3.setChineae(91);
+        stu3.setEnglish(62);
+        stu3.setMath(89);
+        stu3.back();
+        Student20 stu4 = new Student20();
+        stu4.setName("E");
+        stu4.setNumber("000005");
+        stu4.setChineae(92);
+        stu4.setEnglish(68);
+        stu4.setMath(95);
+        stu4.back();
+    }
+
+}
